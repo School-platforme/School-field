6bcb4f5afb553af9e58bc0403aa8113283c4ee30
firas-debich <debichf02@gmail.com>
2021-10-05 19:29:19 +0100

    data base done

--- School/server/School/SchoolRouter.js
+++
@@ -1,9 +0,0 @@
-var pokemonRouter = require('express').Router();
-var pokemonController = require('./pokemonController');
-
-// TODO: Create route handlers for each of the six methods in pokemonController
-pokemonRouter.route('/')
-
-pokemonRouter.route('/:number')
-
-module.exports = pokemonRouter;
