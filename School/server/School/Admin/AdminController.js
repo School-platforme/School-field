5505be52c327d3b5421ddd968d935dfa04e0074d
firas-debich <debichf02@gmail.com>
2021-10-06 10:25:29 +0100

    Auth Admin done back-end part

--- School/server/School/Admin/AdminController.js
+++ School/server/School/Admin/AdminController.js
@@ -1,18 +1,46 @@
-
+var Auth = require("./AuthAdmin")
 var School = require("../School")
 //
-exports.createAdmin = function (req, res) {
+exports.createAdmin = (req, res) => {
     // taking the information of the admin 
-    let AdminData = req.body
-    //save it into the data base 
-    School.AdminModel.create(AdminData)
-        .then(() => {
-            console.log("admin Created")
+    // and use distructing to take the password 
 
-        })
-        //if there is an error 
-        .catch((err) => {
-            console.log(err)
-        })
+    let { User, Password, Email } = req.body
+    /// hash the password 
+    Auth.HashPass(Password).then(Hashed => {
+        //save it into the data base  with the hashed password 
+        School.AdminModel.create({ User, Password: Hashed, Email })
+            .then(() => {
+                res.status(201).send("admin Created")
+            })
+            //if there is an error 
+            .catch((err) => {
+                res.send(404).send(err)
+            })
+    })
 
 };
+
+exports.CheckIfThePassRight = (req, res) => {
+    let AdminId = req.params.id
+    let { User, Password, Email } = req.body
+    School.AdminModel.findById(AdminId, (err, result) => {
+        let hashedPass = result.Password
+        let UserNameFromDataBase = result.User
+        let EmailFromDataBase = result.Email
+        if (UserNameFromDataBase === User && EmailFromDataBase === Email) {
+            Auth.comparePass(Password, hashedPass).then(bool => {
+                console.log(bool)
+                if (bool) {
+                    res.status(201).send("welcome your data is match the data from data base ")
+                } else {
+                    res.status(404).send("check your password ")
+                }
+            })
+        } else {
+            res.status(404).send("NOT FOUND")
+        }
+
+    })
+
+}
--- School/server/School/Admin/AdminRouter.js
+++ School/server/School/Admin/AdminRouter.js
@@ -4,6 +4,10 @@ var AdminController = require("../Admin/AdminController")
 //
 AdminRoute.route('/admin')
     .post(AdminController.createAdmin)
+AdminRoute.route('/admin/:id')
+    .post(AdminController.CheckIfThePassRight)
+
+
 
 
 module.exports = AdminRoute
