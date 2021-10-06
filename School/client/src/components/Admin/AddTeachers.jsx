67fb2cf5808336c5866ea2063a47d8b8594ea696
Imad el Ayoubi <imad.ayoubi72@gmail.com>
2021-10-06 11:41:50 +0100

    admin side updated

--- School/client/src/components/Admin/AddTeachers.jsx
+++ School/client/src/components/Admin/AddTeachers.jsx
@@ -10,9 +10,10 @@ class AddTeachers extends Component {
       TeacherLastName: "",
       Email: "",
       password: "",
-      imageUrl: "",
+      ImageUrl: "",
       Field: "",
-      Phone: 0,
+      Phone: "",
+      Experience: "Beginner",
       teacher: {},
     };
 
@@ -31,10 +32,18 @@ class AddTeachers extends Component {
         TeacherLastName: this.state.TeacherLastName,
         Email: this.state.Email,
         password: this.state.password,
-        imageUrl: this.state.imageUrl,
+        ImageUrl: this.state.ImageUrl,
         Field: this.state.Field,
         Phone: Number(this.state.Phone),
+        Experience: this.state.Experience,
       },
+    });
+  }
+
+  selectExp(e) {
+    console.log("hh", e.target.value);
+    this.setState({
+      Experience: e.target.value,
     });
   }
 
@@ -51,60 +60,80 @@ class AddTeachers extends Component {
   }
 
   render() {
+    console.log("experience", this.state.Experience);
     return (
       <div className="create">
         <div className="create-editor">
           <h2>Create teacher</h2>
           <div className="create-teacher-inputs">
+            <label htmlFor="TeacherName"> First Name </label>
             <input
-              className="create-body-textarea"
+              // className="create-body-textarea"
               name="TeacherName"
               type="text"
               placeholder="First Name"
               onChange={this.handleChange}
             ></input>
+
+            <label htmlFor="TeacherLastName"> Last Name </label>
+
             <input
-              className="create-body-textarea"
+              // className="create-body-textarea"
               name="TeacherLastName"
               type="text"
               placeholder="Last Name"
               onChange={this.handleChange}
             ></input>
+            <label htmlFor="ImageUrl"> Teacher image </label>
+
             <input
-              className="create-body-textarea"
-              name="imageUrl"
+              // className="create-body-textarea"
+              name="ImageUrl"
               type="text"
               placeholder="Teacher image"
               onChange={this.handleChange}
             ></input>
+            <label htmlFor="Field"> Teacher Field </label>
+
             <input
-              className="create-body-textarea"
+              // className="create-body-textarea"
               name="Field"
               type="text"
               placeholder="Teacher Field"
               onChange={this.handleChange}
             ></input>
+            <label htmlFor="Phone"> Phone Number </label>
+
             <input
-              className="create-body-textarea"
-              name="age"
+              // className="create-body-textarea"
+              name="Phone"
               type="text"
-              placeholder="Teacher age"
+              placeholder="Phone number"
               onChange={this.handleChange}
             ></input>
+            <label htmlFor="Email"> Email</label>
+
             <input
-              className="create-body-textarea"
+              // className="create-body-textarea"
               name="Email"
               type="text"
               placeholder="Email"
               onChange={this.handleChange}
             />
+            <label htmlFor="password"> Password</label>
             <input
-              className="create-body-textarea"
+              // className="create-body-textarea"
               name="password"
               type="password"
               placeholder="Enter password"
               onChange={this.handleChange}
             />
+            <label> Years of experience</label>
+            <select name="Experience" onChange={this.selectExp.bind(this)}>
+              <option value="beginner">0 - 2 years</option>
+              <option value="experienced">2 - 4 years</option>
+              <option value="advanced ">More than 4 years</option>
+            </select>
 
             <br></br>
             <br></br>
