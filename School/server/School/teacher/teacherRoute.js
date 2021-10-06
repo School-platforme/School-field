983c964f52e1b4344a34b4e2633b1653c681d605
firas-debich <debichf02@gmail.com>
2021-10-06 17:28:39 +0100

    done

--- School/server/School/teacher/teacherRoute.js
+++ School/server/School/teacher/teacherRoute.js
@@ -5,10 +5,13 @@ var teacherController = require("../teacher/teacherController");
 
 teacherRoute.route('/teacher')
     .post(teacherController.createTeacher)
-
+    .get(teacherController.getAllTeachersAndStudent)
     
-teacherRoute.route('/teacher/:id')
+    
+    teacherRoute.route('/teacher/:id')
     .get(teacherController.getAllStudent)
+    .delete(teacherController.deleteTeacher)
+    .put(teacherController.updateTeacher)
 
 
 module.exports = teacherRoute;
